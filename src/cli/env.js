f941fcfa267349d30d894efc3b9eaa8d1d439df9
Daniel Lazarevic <lazer3112@gmail.com>
2022-12-05 14:26:03 +0200

    feat: task env done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = Object.entries(process.env)
+    .filter((entry) => {
+        const [key] = entry;
+        if (key.includes('RSS_')) return entry;
+    })
+    .map((entry) => entry.join('='));
+    console.log(env.join('; ')); 
 };
-
 parseEnv();
