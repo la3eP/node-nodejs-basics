ee5c63097c07ea92d1420543bec5f3115af37de4
Daniel Lazarevic <lazer3112@gmail.com>
2022-12-05 21:23:11 +0200

    feat: Completed workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import os from "os";
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const resultArr = [];
+    const cpuNum = os.cpus().length
+    for ( let i = 0; i < cpuNum; i++){
+        const promise = new Promise((resolve, reject) => {
+        const worker = new Worker(path.join(__dirname, 'worker.js'),{workerData:20+i});
+        worker.on("message", function(msg){
+            resolve({status:'resolved', data:msg});
+        });
+        worker.on("error", function(){
+            resolve({status:'error', data:null});
+        });
+    });
+    resultArr.push(promise);
+    };
+    const array = await Promise.allSettled(resultArr);
+    for (const item of array)
+        console.log(item.value);
 };
 
 await performCalculations();
