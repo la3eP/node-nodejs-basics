048b8a0f07a1687d2cc4188b6c5d8e33944e80d2
Daniel Lazarevic <lazer3112@gmail.com>
2022-12-05 21:44:13 +0200

    feat: Completed task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,23 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'child_process';
+import * as url from 'url';
+import path from 'path';
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+export const spawnChildProcess = async (args) => {
+  const node = spawn('node', [path.join(__dirname, 'files', 'script.js'), args]);
+
+  process.stdin.pipe(node.stdin);
+  node.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+  node.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  node.stdout.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+  });
+  node.stdout.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
 spawnChildProcess();
